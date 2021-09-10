8d38e66b3315e36e385ed0e1eb59a33015c9f7e0
kcotten <kris.cotten@gmail.com>
2021-09-09 17:51:31 -0700

    ..

--- arrays.go
+++ arrays.go
@@ -85,3 +85,88 @@ func zeros(nums []int) bool {
 	}
 	return true
 }
+
+func kSum(nums []int, target, start, k int) [][]int {
+	result := make([][]int, 0)
+	if start == len(nums) || nums[start]*k > target || target > nums[len(nums)-1]*k {
+		return result
+	}
+	if k == 2 {
+		return twoSum(nums, target, start)
+	}
+
+	for i := start; i < len(nums); i++ {
+		if i == start || nums[i-1] != nums[i] {
+			for _, subset := range kSum(nums, target-nums[i], i+1, k-1) {
+				subset = append([]int{nums[i]}, subset...)
+				result = append(result, subset)
+				//result[len(result)-1] = append(result[len(result)-1], subset...)
+			}
+		}
+
+	}
+	return result
+}
+
+// return index
+func twoSum(nums []int, target, start int) [][]int {
+	left, right := start, len(nums)-1
+	result := make([][]int, 0)
+	for left < right {
+		currSum := nums[left] + nums[right]
+		if currSum < target || (left > start && nums[left] == nums[left-1]) {
+			left++
+		} else if currSum > target || (right < len(nums)-1 && nums[right] == nums[right+1]) {
+			right--
+		} else {
+			result = append(result, []int{nums[left], nums[right]})
+			left++
+			right--
+		}
+	}
+	return result
+}
+
+// return index
+func twoSumIdx(nums []int, target int) []int {
+	idx1, idx2 := 0, 0
+	memo := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		if val, ok := memo[target-nums[i]]; ok && i != val {
+			idx1 = i
+			idx2 = val
+		} else {
+			memo[nums[i]] = i
+		}
+	}
+	return []int{idx1, idx2}
+}
+
+// backtrack
+func combinationSum(candidates []int, target int) [][]int {
+	results := make([][]int, 0)
+	combo := make([]int, 0)
+	var backtrack func(int, int, []int)
+	backtrack = func(remain int, start int, combo []int) {
+		if remain == 0 {
+			// use temp buffer due to next loop operation modifying combo and therefore result
+			temp := make([]int, len(combo))
+			copy(temp, combo)
+			results = append(results, temp)
+			return
+		} else if remain < 0 {
+			return
+		}
+
+		for i := start; i < len(candidates); i++ {
+			combo = append(combo, candidates[i])
+			// 1st call: target - the item we are currently looking at
+			// Subsequently: subtract until we reach the target at beginning of func
+			backtrack(remain-candidates[i], i, combo)
+			combo = combo[:len(combo)-1]
+		}
+	}
+
+	backtrack(target, 0, combo)
+	return results
+}
