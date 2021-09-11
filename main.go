456b915fc30316389d3d6d5abe4553fbb8d4752f
kcotten <kris.cotten@gmail.com>
2021-09-11 11:37:49 -0700

    Add solutions

--- main.go
+++ main.go
@@ -5,12 +5,17 @@ import (
 )
 
 func main() {
-	fmt.Println("Running Test")
+	fmt.Println("Running Array Tests")
 	target := 0
 	nums := []int{1, 0, -1, 0, -2, 2}
 	fmt.Println(fourSum(nums, target))
 	nums = []int{0, 0, 0, 0}
 	fmt.Println(fourSum(nums, target))
+
+	fmt.Println("==================================================")
+	fmt.Printf("Running Strings Tests\n")
+	fmt.Printf("All Unique: %v\n", unique("hello"))
+	fmt.Printf("All Unique: %v\n", unique("helios"))
 }
 
 type TreeNode struct {
--- strings.go
+++ strings.go
@@ -6,3 +6,39 @@ func nextChar(ch byte, n int) byte {
 	}
 	return ch
 }
+
+func unique(s string) bool {
+	memo := make(map[rune]int)
+	for _, r := range s {
+		if _, ok := memo[r]; !ok {
+			memo[r]++
+		} else {
+			return false
+		}
+	}
+	return true
+}
+
+func uniqueNoSpace(s string) bool {
+	for i := 0; i < len(s)-1; i++ {
+		for j := i + 1; j < len(s); j++ {
+			if s[i] == s[j] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func reverseCstyle(s string) string {
+	if len(s) <= 2 {
+		return s
+	}
+	b := make([]byte, len(s))
+	size := len(s) - 2
+	for i := 0; i < size; i++ {
+		b[i], b[size-i] = s[size-i], s[i]
+	}
+	b[len(s)-1] = '\000'
+	return string(b)
+}
